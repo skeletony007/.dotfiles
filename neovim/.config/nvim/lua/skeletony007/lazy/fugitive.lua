a5ed389c90a365591fef41567936efbf2d947f4d
skeletony007 <skeletony007@users.noreply.github.com>
2024-08-11 17:35:02 +0100

    feat: Vim Fugitive Git autostash keybind

--- neovim/.config/nvim/lua/skeletony007/lazy/fugitive.lua
+++ neovim/.config/nvim/lua/skeletony007/lazy/fugitive.lua
@@ -18,7 +18,7 @@ return {
 
                 vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end, opts)
 
-                vim.keymap.set("n", "<leader>P", function() vim.cmd.Git("pull --rebase") end, opts)
+                vim.keymap.set("n", "<leader>P", function() vim.cmd.Git("pull --rebase --autostash") end, opts)
 
                 vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
             end,
