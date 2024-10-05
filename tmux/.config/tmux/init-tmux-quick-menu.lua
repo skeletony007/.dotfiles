97b3f2b71a0b8440452ca9bab5b820c52d5c3eb5
skeletony007 <skeletony007@users.noreply.github.com>
2024-10-05 14:36:54 +0100

    feat(tmux): tmux-quick-menu.lua disable swapfile, backup, and undofile

--- tmux/.config/tmux/init-tmux-quick-menu.lua
+++ tmux/.config/tmux/init-tmux-quick-menu.lua
@@ -1,6 +1,11 @@
-vim.keymap.set("n", "q", "<cmd>q!<CR>")
-vim.keymap.set("n", "<Esc>", "<cmd>q!<CR>")
-vim.keymap.set("n", "<CR>", "<cmd>.w !xargs tmux-sessionizer<CR>:wq<CR>")
+vim.opt_local.swapfile = false
+vim.opt_local.backup = false
+vim.opt_local.undofile = false
+
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
 vim.opt_local.laststatus = 0
+
+vim.keymap.set("n", "q", "<cmd>q!<CR>")
+vim.keymap.set("n", "<Esc>", "<cmd>q!<CR>")
+vim.keymap.set("n", "<CR>", "<cmd>.w !xargs tmux-sessionizer<CR>:wq<CR>")
