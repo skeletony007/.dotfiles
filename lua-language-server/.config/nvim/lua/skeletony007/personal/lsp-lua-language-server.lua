fbb7264975019f7fa0c8c8127b8cc609b907642d
skeletony007 <skeletony007@users.noreply.github.com>
2024-10-16 14:05:59 +0100

    feat(neovim): lua-language-server project specific config
    <https://github.com/VonHeikemen/lsp-zero-docs/blob/a451eee792c41a3b7c8ad9a5744e7839b8ab2b9d/content/v3.x/guide/neovim-lua-ls.md>

--- lua-language-server/.config/nvim/lua/skeletony007/personal/lsp-lua-language-server.lua
+++ lua-language-server/.config/nvim/lua/skeletony007/personal/lsp-lua-language-server.lua
@@ -6,20 +6,6 @@ return {
             ensure_installed = {
                 "lua_ls",
             },
-            handlers = {
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                            },
-                        },
-                    })
-                end,
-            },
         },
     },
 }
