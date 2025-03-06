e139896992cfd060af0a65255547799f657f0e2a
skeletony007 <skeletony007@users.noreply.github.com>
2025-03-06 17:31:46 +0000

    feat(neovim): add nvim-treesitter-context

--- neovim/.config/nvim/lua/skeletony007/lazy/treesitter.lua
+++ neovim/.config/nvim/lua/skeletony007/lazy/treesitter.lua
@@ -1,7 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
+    {
+        "nvim-treesitter/nvim-treesitter",
 
-    build = ":TSUpdate",
+        build = ":TSUpdate",
 
-    config = function() require("nvim-treesitter.configs").setup(_G.personal.nvim_treesitter.setup) end,
+        config = function() require("nvim-treesitter.configs").setup(_G.personal.nvim_treesitter.setup) end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+
+        config = function() require("treesitter-context").setup({ enable = false }) end,
+    },
 }
