4f98bacb50d29c358390634d39f7443147298232
skeletony007 <skeletony007@users.noreply.github.com>
2025-03-25 23:59:27 +0000

    fix(neovim): plugin git-help nvim-lua/plenary.nvim dependency

--- neovim/.config/nvim/lua/skeletony007/lazy/git-help.lua
+++ neovim/.config/nvim/lua/skeletony007/lazy/git-help.lua
@@ -1,6 +1,10 @@
 return {
     "skeletony007/git-help.nvim",
 
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+
     dir = os.getenv("PERSONAL") .. "/git-help.nvim",
 
     config = true,
