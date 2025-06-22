22240501aeddf2af6a66e7e03a15fff76b4683bf
skeletony007 <skeletony007@users.noreply.github.com>
2025-06-22 15:55:09 +0100

    feat(tmux, neovim): harpoon keymaps and extended Tmux navigation

--- neovim/.config/nvim/lua/skeletony007/lazy/harpoon.lua
+++ neovim/.config/nvim/lua/skeletony007/lazy/harpoon.lua
@@ -11,16 +11,15 @@ return {
 
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader><C-d>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>a<C-h>", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader>a<C-j>", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader>a<C-k>", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader>a<C-l>", function() harpoon:list():replace_at(4) end)
     end,
 }
