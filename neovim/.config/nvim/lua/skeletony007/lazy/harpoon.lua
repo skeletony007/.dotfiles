7d19acc260ae0272b23db56803f51a379503ed7a
skeletony007 <skeletony007@users.noreply.github.com>
2025-11-25 08:49:13 +0000

    feat(tmux, neovim): harpoon and tmux quick menu things

--- neovim/.config/nvim/lua/skeletony007/lazy/harpoon.lua
+++ neovim/.config/nvim/lua/skeletony007/lazy/harpoon.lua
@@ -1,3 +1,7 @@
+local function harpoon_notify(n)
+    vim.notify(string.format("%s added to Harpoon %d", vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":."), n))
+end
+
 return {
 
     "theprimeagen/harpoon",
@@ -17,9 +21,21 @@ return {
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>a<C-h>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader>a<C-j>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader>a<C-k>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader>a<C-l>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>a<C-h>", function()
+            harpoon:list():replace_at(1)
+            harpoon_notify(1)
+        end)
+        vim.keymap.set("n", "<leader>a<C-j>", function()
+            harpoon:list():replace_at(2)
+            harpoon_notify(2)
+        end)
+        vim.keymap.set("n", "<leader>a<C-k>", function()
+            harpoon:list():replace_at(3)
+            harpoon_notify(3)
+        end)
+        vim.keymap.set("n", "<leader>a<C-l>", function()
+            harpoon:list():replace_at(4)
+            harpoon_notify(4)
+        end)
     end,
 }
