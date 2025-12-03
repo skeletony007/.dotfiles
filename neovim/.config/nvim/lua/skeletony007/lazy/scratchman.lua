b8ce38fee5d1704270c6734833aa9f25f9a07bfa
skeletony007 <skeletony007@users.noreply.github.com>
2025-12-03 21:45:28 +0000

    feat(neovim): scratchman.nvim lazy loading

--- neovim/.config/nvim/lua/skeletony007/lazy/scratchman.lua
+++ neovim/.config/nvim/lua/skeletony007/lazy/scratchman.lua
@@ -1,6 +1,11 @@
 return {
     "skeletony007/scratchman.nvim",
 
+    cmd = {
+        "Scratch",
+        "ScratchFork",
+    },
+
     dir = os.getenv("PERSONAL") .. "/scratchman.nvim",
 
     config = true,
